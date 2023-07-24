71da265eb01d9a5592fc6ba8258e1629b5cc17dd
Ilya <iljafff@mail.ru>
2023-07-25 02:27:22 +0300

    семинар 1

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+int intnumber = 25;
+double doubleNumber = 12.12;
+string text = "some text";
+bool;   
+
+
+int num = 5;
+Console.WriteLine("num " + num); 
+*/
+// Написать программу котораяя, на вход принимает два числа и приверяет,является ли первое число квадратом вторго.
+//
+// Console.Write("input a first number:  ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("input a second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int quad2 = num2 * num2;
+
+// if (num1 == quad2)
+// {
+//     Console.WriteLine("yes");
+// }
+// else
+// {
+//     Console.WriteLine("no");
+// }
+// Напишите программу которая, на выходы принимает положительное число (N), а на выходы показывает все целые числа в промежутке от -N до N.
+
+// Console.Write("input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int current = -num;
+
+// while(current <= num)
+// {
+//     Console.Write(current + " ");
+//     current++;
+// }
+
